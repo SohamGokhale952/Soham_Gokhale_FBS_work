9d6506f77de6241a4c253b936383896fc77185f2
SOHAM GOKHALE <141512951+SohamGokhale952@users.noreply.github.com>
2025-06-25 20:41:01 +0530

    Add files via upload

---
+++ Firstbit/avg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a =10;
+    float b =20;
+    float c =30;
+    float d =40;
+    float e =50;
+    float avg=(a+b+c+d+e)/5;
+    printf("average is %f",avg);
+    
+}
