7746e155deaef721bebb1464ed9fa9c7c1742f12
SOHAM GOKHALE <141512951+SohamGokhale952@users.noreply.github.com>
2025-06-27 18:15:02 +0530

    Add files via upload

---
+++ C_module/Assignment0/c program/area.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    float r=10;
+float area;
+area=3.14*r*r;
+printf("%f",area);
+}
---
+++ C_module/Assignment0/c program/lab_prac.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+int b = printf("%d", a = a--);
+printf(" %d %d",a,b);
+
+}
