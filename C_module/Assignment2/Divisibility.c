0438d060bb7b8ca7283211f510fcda429227f751
SOHAM GOKHALE <141512951+SohamGokhale952@users.noreply.github.com>
2025-07-01 21:07:13 +0530

    Add files via upload

---
+++ C_module/Assignment2/Divisibility.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main(){
+int no;
+printf("Enter the no: ");
+scanf("%d",&no);
+
+if (no%3==0 && no%5==0)
+{
+    /* code */
+    printf("divisible by both");
+    
+}
+else
+{
+if (no%3==0)
+{
+printf("no is divisible by 3 not by 5");
+}
+else if (no%5==0)
+{
+printf("no is divisible by 5 not by 3");
+}
+else
+{
+printf("divisible by none");
+}
+}
+
+
+
+
+}
---
+++ C_module/Assignment2/age.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main(){
+int age;
+printf("Enter the age: ");
+scanf("%d",&age);
+
+if (age<12)
+{
+    printf("You are child");
+}
+else if (age>=12 && age<=19)
+{
+        printf("You are teenager");
+
+}
+else if(age>=20 && age<=59)
+{
+    printf("You are Adult");
+    /* code */
+}
+else
+{
+        printf("You are Senior");
+
+}
+
+
+
+
+
+
+}
