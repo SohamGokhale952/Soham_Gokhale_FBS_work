c9baf67cea10a16927bbc072b36256e55e3f312c
SOHAM GOKHALE <141512951+SohamGokhale952@users.noreply.github.com>
2025-07-10 18:38:44 +0530

    Add files via upload

---
+++ C_module/Assignment8/assign2/q6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void result(int);
+void main(){
+      int marks;
+printf("Enter the Marks: ");
+scanf("%d",&marks);
+   result(marks);
+
+}
+
+void result(int marks){
+   
+
+if (marks>=40)
+{
+    if (marks>75)
+    {
+        printf("distinction");
+    }
+    else if (marks>65)
+    {
+        printf("First Class");
+    }
+    else if (marks>55)
+    {
+        printf("Second Class");
+    }
+    else if (marks>=40)
+    {
+        printf("Pass Class");
+    }
+    
+    
+}
+
+else
+{
+    printf("Fail");
+}
+
+
+}
