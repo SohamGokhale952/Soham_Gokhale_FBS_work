e35685fac3e62c406789bdf94df17999fae2a52a
SOHAM GOKHALE <141512951+SohamGokhale952@users.noreply.github.com>
2025-07-14 19:25:23 +0530

    Add files via upload

---
+++ C_module/Assignment10/Assignment4/q3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void perfectno(int*);
+int main(){
+     int no;
+printf("Enter the end point");
+scanf("%d",&no);
+    perfectno(&no);
+
+}
+void perfectno(int* no){
+   int sum=0;
+int i=1;
+
+for (; i < *no;i++ )
+{
+if (*no%i==0)
+   {
+       /* code */
+       sum=sum+i;
+   }
+   if (sum==*no)
+{
+    /* code */printf("%d is strong no\n",i);
+}
+   }
+
+
+// else
+// {
+//     printf("not perfect no");
+// }
+
+}
